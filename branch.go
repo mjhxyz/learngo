8ebd0b9dbc554f4f445a58290e15e3fec13bd8d3
Mao <mjhxyz@foxmail.com>
2023-08-09 13:45:38 +0800

    update: branch.go

--- branch.go
+++ branch.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	const filename = "abc.txt"
+	if contents, err := os.ReadFile(filename); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s\n", contents)
+	}
 }

48534d6be2a2ad0c574b5cfacd52b72df51b618d
Mao <mjhxyz@foxmail.com>
2023-08-09 13:50:57 +0800

    update: branch.go

--- branch.go
+++ branch.go
@@ -5,6 +5,23 @@ import (
 	"os"
 )
 
+func grade(score int) string {
+	g := ""
+	switch {
+	case score < 0 || score > 100:
+		panic(fmt.Sprintf("Wrong score: %d", score))
+	case score < 60:
+		g = "F"
+	case score < 80:
+		g = "C"
+	case score < 90:
+		g = "B"
+	case score <= 100:
+		g = "A"
+	}
+	return g
+}
+
 func main() {
 	const filename = "abc.txt"
 	if contents, err := os.ReadFile(filename); err != nil {
@@ -12,4 +29,14 @@ func main() {
 	} else {
 		fmt.Printf("%s\n", contents)
 	}
+
+	fmt.Println(
+		grade(0),
+		grade(59),
+		grade(60),
+		grade(82),
+		grade(99),
+		grade(100),
+		//grade(101),
+	)
 }

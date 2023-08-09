84678e644e129659da824ce1e60398589ed4ee82
mjhxyz <mjhxyz@foxmail.com>
2023-08-10 00:30:24 +0800

    update: node.go

---
+++ tree/node.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type treeNode struct {
+	value       int
+	left, right *treeNode
+}
+
+func main() {
+	//var root treeNode
+	root := treeNode{value: 3}
+	root.left = &treeNode{}
+	root.right = &treeNode{5, nil, nil}
+	root.right.left = new(treeNode)
+	fmt.Println(root)
+}

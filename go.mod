419058ae338d63fbe8715c65233cfa92ec99db99
mjhxyz <mjhxyz@foxmail.com>
2023-08-13 14:15:50 +0800

    update: go mod

--- go.mod
+++ go.mod
@@ -6,5 +6,9 @@ require go.uber.org/zap v1.25.0
 
 require (
 	github.com/stretchr/testify v1.8.3 // indirect
+	github.com/yuin/goldmark v1.4.13 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/tools v0.12.0 // indirect
 )
--- queue/queue.go
+++ queue/queue.go
@@ -1,19 +1,23 @@
 package queue
 
-type E interface{}
-type Queue []E
+// Queue 一个先进先出的队列
+type Queue []int
 
+// Push 将元素添加到队列中
+//
+//	e.g. q.Push(123)
 func (q *Queue) Push(v int) {
 	*q = append(*q, v)
 }
 
+// Pop 将元素从队列中移除
 func (q *Queue) Pop() int {
 	head := (*q)[0]
 	*q = (*q)[1:]
-	// 强制转换
-	return head.(int)
+	return head
 }
 
+// IsEmpty 判断队列是否为空
 func (q *Queue) IsEmpty() bool {
 	return len(*q) == 0
 }
